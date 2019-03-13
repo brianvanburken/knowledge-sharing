b95e76771fc8e2b7533a210804f0c6b7c0b09446
Brian van Burken <brianvanburken@gmail.com>
2019-03-13 08:47:12 +0100

    Add challenge 5

---
+++ challenge5/README.md
@@ -0,0 +1,21 @@
+# Challenge5
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `challenge5` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:challenge5, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/challenge5](https://hexdocs.pm/challenge5).
+
---
+++ challenge5/lib/challenge5.ex
@@ -0,0 +1,23 @@
+defmodule Challenge5 do
+  def loop() do
+    secret = "AVISI-85hsd715"
+    key = "AVISI-359gdsh249tnh9"
+    final_key = "AVISI-235hagy4"
+    len = String.length(key) - 1
+    receive do
+      {^secret, ^key, client} ->
+        send(client, "Well done! Here is the final key: " <> final_key)
+      {^secret, n, client} ->
+        :timer.sleep(1000 * 3)
+        if n > len do
+          send(client, {:error, "You can't ask a position past the length of the key."}
+        else
+          part = String.at(key, n)
+          send(client, {:ok, part})
+        end
+      {_, _, client} -> send(client, {:error, "Something went wrong. Did you send the correct key?"}
+      _ -> send(client, {:error, "Something went wrong. Did you send the correct message?"}
+    end
+    loop()
+  end
+end
