3d45b01bb1b857e27d77eb6a0fea7033a576ad3a
Brian van Burken <brianvanburken@gmail.com>
2019-03-27 10:20:07 +0100

    Update key for challenge 3

--- challenge3/generate.ex
+++ challenge3/generate.ex
@@ -1,42 +1,34 @@
 defmodule Generate do
-  # Generate.create_bin("challenge2.bin")
-  def create_bin(file_name) do
-    # our key
-    key = "AVISI-dJ1Neg"
-
-    # we destruct our key into the specific parts
+  def create_bin(file_name, key) when byte_size(key) == 26 do
+    # we destruct our key into the specific parts, length = 26
     <<
-    a::binary-size(2), # AV
-    b::binary-size(1), # I
-    c::binary-size(3), # SI-
-    d::binary-size(1), # d
-    e::binary-size(1), # J
-    f::binary-size(1), # 1
-    g::binary-size(3) # Neg
+    a::binary-size(5),
+    b::binary-size(1),
+    c::binary-size(3),
+    d::binary-size(7),
+    e::binary-size(4),
+    f::binary-size(2),
+    g::binary-size(4)
     >> = key
 
     # we generate the binary string with random data
     binary_data =
-      random_string(54)
+      :crypto.strong_rand_bytes(54)
       <> d <> # 3
-      random_string(2)
+      :crypto.strong_rand_bytes(2)
       <> e <> # 4
-      random_string(2)
-      <> f <> # 5
-      random_string(30)
+      :crypto.strong_rand_bytes(2)
       <> g <> # 6
-      random_string(30)
+      :crypto.strong_rand_bytes(30)
+      <> f <> # 5
+      :crypto.strong_rand_bytes(30)
       <> a <> # 0
-      random_string(1)
+      :crypto.strong_rand_bytes(1)
       <> b <> # 1
-      random_string(10)
+      :crypto.strong_rand_bytes(10)
       <> c <> # 2
-      random_string(102)
+      :crypto.strong_rand_bytes(102)
 
-    # here we write the binary_data to a file
-    :ok = File.write(file_name, binary_data, [:binary])
+    File.write(file_name, binary_data)
   end
-
-  # Generates a random string for the given length
-  def random_string(length), do: :crypto.strong_rand_bytes(length)
 end
--- challenge3/solution.ex
+++ challenge3/solution.ex
@@ -2,22 +2,25 @@ defmodule Solution do
   def parse(file_name) do
     case File.read(file_name) do
       {:ok, contents} ->
-        <<_::binary-size(54),
-          a::binary-size(1),
-          _::binary-size(2),
-          b::binary-size(1),
-          _::binary-size(2),
-          c::binary-size(1),
-          _::binary-size(30),
-          d::binary-size(3),
-          _::binary-size(30),
-          e::binary-size(2),
-          _::binary-size(1),
-          f::binary-size(1),
-          _::binary-size(10),
-          g::binary-size(3),
-          _::binary>> = contents
-        e <> f <> g <> a <> c <> b <> d
+        <<
+        _::binary-size(54),
+        d::binary-size(7),
+        _::binary-size(2),
+        e::binary-size(4),
+        _::binary-size(2),
+        g::binary-size(4),
+        _::binary-size(30),
+        f::binary-size(2),
+        _::binary-size(30),
+        a::binary-size(5),
+        _::binary-size(1),
+        b::binary-size(1),
+        _::binary-size(10),
+        c::binary-size(3),
+        _::binary-size(102)
+        >> = contents
+
+        a <> b <> c <> d <> e <> f <> g
     end
   end
 end
